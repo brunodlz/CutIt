38ea50f6f1ed12aebd92bed54e9bf016b82eb6ac
Bruno da Luz <brunolabx@gmail.com>
2024-05-26 15:44:09 -0300

    Create ScreenshotView

---
+++ CutIt/Scenes/Screenshot/ScreenshotView.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+struct ScreenshotView: View {
+    @StateObject var viewModel: ScreenCaptureViewModel
+
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(
+                    columns: [GridItem(
+                        .adaptive(
+                            minimum: 200,
+                            maximum: 300
+                        )
+                    )]
+                ) {
+                    ForEach(viewModel.images, id: \.self) { image in
+                        Image(nsImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(RoundedRectangle(cornerRadius: 16))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 16)
+                                    .stroke(.mint, lineWidth: 2)
+                            }
+                            .padding(EdgeInsets(top: 2,
+                                                leading: 2,
+                                                bottom: 2,
+                                                trailing: 2))
+
+                    }
+                }
+            }
+            .padding(EdgeInsets(top: 2,
+                                leading: 2,
+                                bottom: 2,
+                                trailing: 2))
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ScreenshotView(viewModel: .init())
+}
